66598c1afe4420f1265a0e5a70d5f91e2860fbae
Umut Ekici <umut_ekici10@hotmail.com>
2025-08-04 20:45:09 +0300

    Setup MongoDB connection and add initial entities

--- ContactMicroservice/Domain/Entities/Person.cs
+++ ContactMicroservice/Domain/Entities/Person.cs
@@ -1,7 +1,12 @@
-﻿namespace ContactMicroservice.Domain.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ContactMicroservice.Domain.Entities
 {
     public class Person
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public Guid Id { get; set; }
 
         public string FirstName { get; set; }
--- ContactMicroservice/Program.cs
+++ ContactMicroservice/Program.cs
@@ -1,6 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ContactMicroservice.Infrastructure.Persistence;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+var app = builder.Build();
 
 app.Run();
