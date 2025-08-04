5f51b0a217379ef8b96b4846233cac21de955cee
Umut Ekici <umut_ekici10@hotmail.com>
2025-08-04 23:41:04 +0300

    Configure MongoDB for Report microservice

---
+++ ReportMicroservice/Infrastructure/Persistence/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ReportMicroservice.Infrastructure.Persistence
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
--- ReportMicroservice/Program.cs
+++ ReportMicroservice/Program.cs
@@ -1,6 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ReportMicroservice.Infrastructure.Persistence;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<MongoDbSettings>(
+    builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<IMongoClient>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddScoped<IMongoDatabase>(sp =>
+{
+    var settings = sp.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+    var client = sp.GetRequiredService<IMongoClient>();
+    return client.GetDatabase(settings.DatabaseName);
+});
+
+var app = builder.Build();
 
 app.Run();
