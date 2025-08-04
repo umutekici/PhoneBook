b2b8098f4d8a6d12c3e69a9e58a63b080fb0c79f
Umut Ekici <umut_ekici10@hotmail.com>
2025-08-04 22:28:43 +0300

    fix: make ContactInfos optional in PersonDto and handle null safely in service

--- ContactMicroservice/Application/DTOs/PersonDto.cs
+++ ContactMicroservice/Application/DTOs/PersonDto.cs
@@ -5,7 +5,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Company { get; set; }
-        public List<ContactInfoDto> ContactInfos { get; set; }
+        public List<ContactInfoDto> ContactInfos { get; set; } =  new List<ContactInfoDto>();
     }
 
 }
