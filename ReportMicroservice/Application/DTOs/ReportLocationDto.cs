ed7c71f6de01c63495d90d9d9aead3edc10ed01f
Umut Ekici <umut_ekici10@hotmail.com>
2025-08-05 22:35:41 +0300

    Add controller for creating report requests and add ReportLocationDto

---
+++ ReportMicroservice/Application/DTOs/ReportLocationDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ReportMicroservice.Application.DTOs
+{
+    public class ReportLocationDto
+    {
+        public string Location { get; set; }
+    }
+}
--- ReportMicroservice/Presentation/Controllers/ReportController.cs
+++ ReportMicroservice/Presentation/Controllers/ReportController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using ReportMicroservice.Application.DTOs;
 using ReportMicroservice.Application.Interfaces;
+using ReportMicroservice.Domain.Entities;
+using ReportMicroservice.Domain.Enums;
+using Shared.Messages;
 
 namespace ReportMicroservice.Presentation.Controllers
 {
@@ -8,10 +13,12 @@ namespace ReportMicroservice.Presentation.Controllers
     public class ReportController : ControllerBase
     {
         private readonly IReportService _reportService;
+        private readonly IRequestClient<ILocationReportRequest> _locationReportRequestClient;
 
-        public ReportController(IReportService reportService)
+        public ReportController(IReportService reportService, IRequestClient<ILocationReportRequest> locationReportRequestClient)
         {
             _reportService = reportService;
+            _locationReportRequestClient = locationReportRequestClient;
         }
 
         [HttpGet]
@@ -28,6 +35,30 @@ namespace ReportMicroservice.Presentation.Controllers
             if (report == null)
                 return NotFound();
             return Ok(report);
+        }
+
+        [HttpPost("location-report")]
+        public async Task<IActionResult> RequestReport([FromBody] ReportLocationDto reportLocationDto)
+        {
+            var reportId = Guid.NewGuid();
+            var requestedDate = DateTime.UtcNow;
+
+            await _reportService.CreateReportAsync(new Report
+            {
+                Id = reportId,
+                Location = reportLocationDto.Location,
+                RequestedDate = requestedDate,
+                Status = ReportStatus.Preparing
+            });
+
+            var response = await _locationReportRequestClient.GetResponse<ILocationReportResponse>(new
+            {
+                ReportId = reportId,
+                Location = reportLocationDto.Location,
+                RequestedDate = requestedDate
+            });
+
+            return Ok(response.Message);
         }
     }
 }
